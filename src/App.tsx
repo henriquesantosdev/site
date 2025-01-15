67dec26c5b9fbb4476497817113f4b22d26660f6
henriquesantosdev <henriquesantos2277@gmail.com>
2025-01-14 22:32:26 -0300

    Gerando estrutura de projeto react

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <>
+      <nav className="bg-transparent p-4 flex gap-8 justify-center items-center">
+        <a href="">_Home</a>
+        <a href="">_Sobre mim</a>
+        <a href="">_Hablilidades</a>
+        <a href="">_Projetos</a>
+      </nav>
+    </>
+  )
+}
