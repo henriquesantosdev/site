43c27430710f5155b62279e82db7aa8eff302b65
henriquesantosdev <henriquesantos2277@gmail.com>
2025-01-21 20:27:27 -0300

    Progress to create portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
+import { BicepsFlexed, FolderCode, House, User } from "lucide-react";
+
 export default function App() {
   return (
     <>
-      <nav className="bg-transparent p-4 flex gap-8 justify-center items-center">
-        <a href="">_Home</a>
-        <a href="">_Sobre mim</a>
-        <a href="">_Hablilidades</a>
-        <a href="">_Projetos</a>
-      </nav>
+      <div className="bg-primaryBlue p-4 h-[2000px]">
+        <nav className="bg-secondaryGray/75 backdrop-blur-sm border border-[#2C2C2C] flex justify-center mx-auto w-[1000px] p-2 rounded-xl gap-8 sticky top-4">
+          <a href="#" className="text-quintiaryGray p-2 rounded-lg flex font-semibold gap-2 hover:text-white hover:bg-primaryGray">
+            <House />
+            Home
+          </a>
+
+          <a href="#" className="text-quintiaryGray p-2 rounded-lg flex font-semibold gap-2 hover:text-white hover:bg-primaryGray">
+            <User />
+            Sobre mim
+          </a>
+
+          <a href="#" className="text-quintiaryGray p-2 rounded-lg flex font-semibold gap-2 hover:text-white hover:bg-primaryGray">
+            <BicepsFlexed />
+            Habilidades
+          </a>
+
+          <a href="#" className="text-quintiaryGray p-2 rounded-lg flex font-semibold gap-2 hover:text-white hover:bg-primaryGray">
+            <FolderCode />
+            Projetos
+          </a>
+        </nav>
+        <div className="bg-secondaryGray border border-[#2C2C2C] flex items-center flex-col mx-auto w-[1000px] p-2 pt-8 mt-4 rounded-xl">
+          <div className="bg-zinc-800 flex items-center gap-2 px-4 rounded-full text-secondaryGreen mb-4">
+            <div className="bg-[#64ff1c] w-2 h-2 rounded-full"></div>
+            <span className="font-semibold text-zinc-300 text-sm">Avaliable for Work </span>
+          </div>
+
+          <img
+            src="/avatar.png"
+            alt="Imagem do desenvolvedor Henrique Santos"
+            className="w-56 rounded-full border-[16px] border-solid border-zinc-800"
+          />
+          <h1 className="text-white font-bold text-4xl mt-4">Henrique Santos</h1>
+          <h2 className="text-">Full Stack Developer</h2>
+        </div>
+      </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Inter'
+      },
+      colors: {
+        primaryGray: '#161616',
+        secondaryGray: '#212121',
+        tertiaryGray: '#2C2C2C',
+        quartiaryGray: '#373737',
+        quintiaryGray: '#909090',
+
+        primaryBlue: '#020617',
+        secondaryBlue: '#5276bf',
+        tertiaryBlue: '#171F2D',
+        
+        primaryGreen: '#28C780',
+        secondaryGreen: '#039754',
+        tertiaryGreenTransparence: 'rgba(40, 199, 128, 0.15)',
+      },
+      backgroundImage: "/quadriculado.avif"
+    },
   },
   plugins: [],
 }
