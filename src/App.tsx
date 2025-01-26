3c1b60d771f6d406aef4324e41e1b3e3bd50ec50
henriquesantosdev <henriquesantos2277@gmail.com>
2025-01-26 18:16:33 -0300

    portfolio progress

--- src/App.tsx
+++ src/App.tsx
@@ -112,8 +112,8 @@ export default function App() {
           <section id="home" className="md:bg-intro bg-no-repeat bg-cover h-[calc(100vh-50px)] md:h-[500px] bg-center flex flex-col-reverse items-center gap-10 md:gap-24 justify-center md:flex-row p-6 rounded-xl">
 
             <div className="w-full md:w-5/12">
-              <h1 className="text-white whitespace-nowrap font-bold text-5xl text-center md:text-left">Olá, eu sou <br /> Henrique Santos</h1>
-              <p className="text-sky-500 font-semibold text-2xl mt-2 text-center md:text-left">Fullstack developer</p>
+              <h1 className="text-white whitespace-nowrap font-bold text-[2.75rem]/10 md:text-5xl text-center md:text-left">Olá, eu sou <br /> Henrique Santos</h1>
+              <p className="text-sky-500 font-semibold text-2xl mt-4 md:mt-2 text-center md:text-left">Fullstack developer</p>
 
               <div className="flex gap-2 mt-8">
                 <ButtonComponent>
