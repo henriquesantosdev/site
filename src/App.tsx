abd530ec51ca1826f39fd47144c4960e35035af9
henriquesantosdev <henriquesantos2277@gmail.com>
2025-01-27 23:55:39 -0300

    remove loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 
-import { lazy, Suspense } from "react";
-
-const BuildComponent = lazy(() => import("./components/layout/BuildComponent"))
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { SobreMim } from "./components/layout/SobreMim";
+import { Skills } from "./components/layout/Skills";
+import { SoftSkills } from "./components/layout/SoftSkills";
+import { Projects } from "./components/layout/Projects";
+import { Footer } from "./components/layout/Footer";
+import { MenuComponent } from "./components/layout/MenuComponent";
+import { HomeComponent } from "./components/layout/HomeComponent";
 
 export default function App() {
   return (
-    <Suspense fallback={<div className="w-full h-screen flex items-center justify-center">Carregando...</div>}>
-      <BuildComponent/>
-    </Suspense>
+    <div id="dark-mode-div" className="">
+      <SpeedInsights />
+      
+      <div className="bg-bluev5 dark:bg-neutral-100 p-2 md:p-4">
+        <MenuComponent />
+        <div className="max-w-6xl mx-auto">
+          <HomeComponent />
+        </div>
+
+        <div className="max-w-3xl mx-auto mt-4">
+          <SobreMim />
+          <Skills />
+          <SoftSkills />
+          <Projects />
+          <Footer />
+        </div>
+
+      </div>
+    </div>
   )
 }
