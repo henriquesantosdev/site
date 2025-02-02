001f3d64b1d699e1ea7ef5c4ec0612410d9579fe
henriquesantosdev <henriquesantos2277@gmail.com>
2025-02-02 18:51:01 -0300

    ajustes de ux na sessao de projetos

--- src/components/layout/Projects.tsx
+++ src/components/layout/Projects.tsx
@@ -7,9 +7,9 @@ export const Projects = () => {
   return (
     <section id="projetos" className="mt-10 p-4 rounded-xl text-2xl text-white font-bold scroll-mt-20">
       <div className="mb-6">
-        <h2 className="flex items-center gap-4 font-jet text-left text-xl text-sky-500 dark:text-blue-700">
+        <h2 className="flex items-center flex-wrap-reverse gap-4 font-jet text-left text-xl text-sky-500 dark:text-blue-700">
           /Projetos
-          <TagComponentAlert style="border border-sky-500 dark:text-blue-700 dark:border-blue-700" text="Clique para ver projeto">
+          <TagComponentAlert style="border border-sky-500 dark:text-blue-700 dark:border-blue-700" text="Clique no card para ver o projeto">
             <MousePointerClick className="size-4" />
           </TagComponentAlert>
         </h2>
--- src/components/projects/CardComponentSmartMoney.tsx
+++ src/components/projects/CardComponentSmartMoney.tsx
@@ -2,6 +2,7 @@ import { FaReact } from "react-icons/fa";
 import { TagComponentProjects } from "../TagComponentProjects";
 import avatar from '/avatar.webp'
 import { SiNestjs } from "react-icons/si";
+import { RiTailwindCssFill } from "react-icons/ri";
 
 export const CardComponentSmartMoney = () => {
   return (
@@ -20,9 +21,9 @@ export const CardComponentSmartMoney = () => {
             <div className="text-lg font-extrabold font-jet tracking-wide text-sky-500 uppercase">Smart money (em desenvolvimento)</div>
 
             <p className="mt-2 text-white text-sm font-jet">
-            Smart Money é um app de gestão financeira com ReactJS e NestJS, que ajuda a controlar gastos, acompanhar receitas e visualizar relatórios de forma intuitiva e eficiente.
+              Smart Money é um app de gestão financeira com ReactJS e NestJS, que ajuda a controlar gastos, acompanhar receitas e visualizar relatórios de forma intuitiva e eficiente.
             </p>
-            
+
             <div className="mt-4 flex gap-2 flex-wrap">
               <TagComponentProjects style="border border-nestjs" text="Nestjs">
                 <SiNestjs className="text-nestjs size-4" />
@@ -30,6 +31,10 @@ export const CardComponentSmartMoney = () => {
 
               <TagComponentProjects style="border border-react" text="React">
                 <FaReact className="text-react size-4" />
+              </TagComponentProjects>
+
+              <TagComponentProjects style="border border-tailwind" text="Tailwind">
+                <RiTailwindCssFill className="text-tailwind size-4" />
               </TagComponentProjects>
             </div>
           </div>
